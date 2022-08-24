309c6ef90c420a99c41d1f36b6cce2c0415ee63a
corey cothrum <contact@coreycothrum.com>
2022-08-24 12:58:52 -0500

    refactor: mender-bist-commit.service type=oneshot
    refactor mender-bist-commit.service for more reliability. The
    new ordering added is better suited w/ these changes:
      * make type=oneshot
      * check explicitly for failed systemd services (not is-system-running)
    
    This better lets us set explicit dependencies w/ MENDER/STANDALONE_DAEMON_BIST_SYSTEMD_AFTER

--- recipes-mender/mender-bist-commit/files/mender-bist-commit.sh
+++ recipes-mender/mender-bist-commit/files/mender-bist-commit.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-echo "will check status in @@MENDER/STANDALONE_DAEMON_BIST_DELAY_SECS@@ seconds"
-
-sleep @@MENDER/STANDALONE_DAEMON_BIST_DELAY_SECS@@
-
 rc=0
 
-if systemctl --quiet is-system-running; then
+if ! systemctl --quiet is-failed "*"; then
   echo "boot PASS : committing current mender artifact"
   mender commit
   rc=$?
 else
   echo "boot ERROR: reboot to rollback"
+  rc=1
 fi
 
 #nothing to commit, not an error
