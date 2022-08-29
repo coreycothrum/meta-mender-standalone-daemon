d697460858269715d4287fa12a94aa35eb5d822b
corey cothrum <contact@coreycothrum.com>
2022-08-29 11:11:38 -0500

    refactor(mender-bist-commit): add systemd still starting as abort condition

--- recipes-mender/mender-bist-commit/files/abort-if-bist-in-progress.sh
+++ recipes-mender/mender-bist-commit/files/abort-if-bist-in-progress.sh
@@ -12,8 +12,10 @@ function fatal {
 }
 
 ################################################################################
-if   systemctl --quiet is-active mender-bist-commit.service > /dev/null 2>&1; then
-  fatal "mender-bist-commit.service is still active. Wait longer before you can update again."
+if   systemctl is-system-running | grep -q "initializing\|starting"   > /dev/null 2>&1; then
+  fatal "system has not finish start-up process. Wait longer before you can update."
+elif systemctl --quiet is-active mender-bist-commit.service > /dev/null 2>&1; then
+  fatal "mender-bist-commit.service is still active. Wait longer before you can update."
 elif systemctl --quiet is-failed mender-bist-commit.service > /dev/null 2>&1; then
   fatal "mender-bist-commit.service is in a failed state. A reboot is required to rollback this failed update."
 fi
