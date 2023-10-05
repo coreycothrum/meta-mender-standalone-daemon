e8295ba63771966c77fcd9a370bc1743f1af6bc4
Corey Cothrum <contact@coreycothrum.com>
2023-10-05 09:05:24 -0500

    fix: check upgrade_available before running mender commit
    this was failing after a rollback, leaving mender-bist-commit.service in a bad state

--- recipes-mender/mender-bist-commit/files/mender-bist-commit.sh
+++ recipes-mender/mender-bist-commit/files/mender-bist-commit.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
 rc=0
+UPGRADE_AVAIL="$(fw_printenv upgrade_available | sed 's/[^=]*=//')"
 
-if ! systemctl --quiet is-failed "*"; then
-  echo "boot PASS : committing current mender artifact"
-  mender commit
-  rc=$?
+if [ "$UPGRADE_AVAIL" -ne "0" ]; then
+  echo "mender upgrade_available"
+
+  if ! systemctl --quiet is-failed "*"; then
+    echo "systemctl reports all good; committing current mender artifact"
+    mender commit
+    rc=$?
+  else
+    echo "systemctl reports failures; not commiting current mender artifact; reboot to rollback"
+    rc=1
+  fi
+
+  # redundant now? # nothing to commit, not an error
+  # redundant now? if [ $rc -eq 2 ]; then
+  # redundant now?   rc=0
+  # redundant now? fi
 else
-  echo "boot ERROR: reboot to rollback"
-  rc=1
-fi
-
-#nothing to commit, not an error
-if [ $rc -eq 2 ]; then
-  rc=0
+  echo "no mender upgrade_available; continuing to boot"
 fi
 
 exit $rc
